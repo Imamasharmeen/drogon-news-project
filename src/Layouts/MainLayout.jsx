cd6d77e42e50ab2cedb33bae26ef614620c877dd
Imamasharmeen <imamasharmeen@gmail.com>
2024-11-13 18:52:02 +0200

    Router setup and added Header

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <header>
+                <Header></Header>
+            </header>
+            <nav></nav>
+            <main></main>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import moment from "moment";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="flex flex-col justify-center items-center mt-6 gap-4 border">
+        <div >
+          <img src={logo} className=""></img>
+        </div>
+        <h2>Journalism Without Fear or Favour</h2>
+        <p>{moment().format("dddd, MMMM Do, YYYY")}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
